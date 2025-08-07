90d46447f970b4b46e8f2accc21b0f6b432eed67
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2025-08-08 00:41:19 +0600

    A team problem solved

---
+++ A_Team.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >>n;
+    int a, b, c , ans=0;
+
+    for(int i=0; i<n; i++){
+        cin>> a >> b >> c;
+
+        if(a+b+c >= 2){
+            ans++;
+        }
+    }
+    cout<<ans <<endl;
+
+    return 0;
+}
