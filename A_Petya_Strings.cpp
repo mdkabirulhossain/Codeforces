85f73576cdff540f53c666f47b775fc515588ba8
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2025-08-21 14:40:14 +0600

    A petya String problem solved

---
+++ A_Petya_Strings.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    string s1, s2;
+    cin >> s1 >> s2;
+
+    transform(s1.begin(), s1.end(), s1.begin(), ::toupper);
+    transform(s2.begin(), s2.end(), s2.begin(), ::toupper);
+    
+    if( s1 == s2){
+        cout << 0 <<endl;
+    }
+    else{
+        s1 > s2 ? cout << 1 <<endl : cout << -1 <<endl;
+    }
+    return 0;
+}
