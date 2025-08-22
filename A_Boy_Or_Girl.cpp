ac2a95751beb603bda57da22f72fe23a46e08588
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2025-08-22 23:58:10 +0600

    a boy or girls problem another solutions

--- A_Boy_Or_Girl.cpp
+++ A_Boy_Or_Girl.cpp
@@ -1,28 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-    string s;
-    cin >> s;
-    int count = s.length();
+//Way 1
+// int main()
+// {
+//     string s;
+//     cin >> s;
+//     int count = s.length();
 
-    for (int i = 0; i < s.length() - 1; i++)
-    {
-        for (int j = 1+i; j < s.length(); j++)
-        {
-            if (s[i] == s[j])
-            {
-                count--;
-                break;
-            }
-        }
-    }
+//     for (int i = 0; i < s.length() - 1; i++)
+//     {
+//         for (int j = 1+i; j < s.length(); j++)
+//         {
+//             if (s[i] == s[j])
+//             {
+//                 count--;
+//                 break;
+//             }
+//         }
+//     }
     
-    if(count %2 == 0){
-        cout<<"CHAT WITH HER!"<<endl;
+//     if(count %2 == 0){
+//         cout<<"CHAT WITH HER!"<<endl;
+//     }else{
+//         cout<<"IGNORE HIM!"<<endl;
+//     }
+//     return 0;
+// }
+
+//////////     Way 2
+int main(){
+    string s;
+    cin >>s;
+
+    set<char> uniqueChars(s.begin(), s.end());
+    cout << uniqueChars.size() <<endl;
+    if(uniqueChars.size() % 2 == 0){
+       cout<<"CHAT WITH HER!"<<endl;
     }else{
         cout<<"IGNORE HIM!"<<endl;
     }
-    return 0;
 }
