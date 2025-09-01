94332145657cbed15a61ff5b3dc32dd8bf65a334
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2025-09-01 20:20:03 +0600

    Sort problem is solved

--- Practice.cpp
+++ Practice.cpp
@@ -18,26 +18,49 @@
 // }
 
 // 2. GCD and LCM
-#include <bits/stdc++.h>
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main()
+// {
+
+//     int num1, num2, small, res, GCD =1;
+//     cin >> num1 >> num2;
+//     small = num1 < num2 ? num1 : num2;
+
+//     for(int i= 1; i<= small; i++){
+//         if(num1 % i == 0 && num2 % i == 0){
+//             GCD = i;
+//         }
+//     }
+
+//     int LCM = (num1 * num2) / GCD ;
+
+//     cout <<"GCD: " << GCD <<endl;
+//     cout <<"LCM: " << LCM <<endl;
+
+//     return 0;
+// }
+
+
+// 3 String Reverse and sort
+#include<bits/stdc++.h>
 using namespace std;
 
-int main()
-{
+int main(){
+    string s;
+    cin >> s;
 
-    int num1, num2, small, res, GCD =1;
-    cin >> num1 >> num2;
-    small = num1 < num2 ? num1 : num2;
-
-    for(int i= 1; i<= small; i++){
-        if(num1 % i == 0 && num2 % i == 0){
-            GCD = i;
+    for(int i=0; i<s.length()-1; i++){
+        for(int j= i + 1; j<s.length(); j++){
+            if(s[i] > s[j]){
+                char t = s[i];
+                s[i] = s[j];
+                s[j] = t;
+            }
         }
     }
 
-    int LCM = (num1 * num2) / GCD ;
-
-    cout <<"GCD: " << GCD <<endl;
-    cout <<"LCM: " << LCM <<endl;
-
+    cout << s <<endl;
     return 0;
 }
