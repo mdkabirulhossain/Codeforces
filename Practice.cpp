f190ab7d6a99c65d4ef5432013e0d39d6cda6653
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2025-09-01 19:15:09 +0600

    Reverse problem solved

---
+++ Practice.cpp
@@ -0,0 +1,20 @@
+// 1. Reverse problem 
+
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// int main(){
+//     int n, rev=0, left = 0;
+//     cin >>n;
+
+//     while (n > 0){
+//         left = n % 10;
+//         rev = rev * 10 + left;
+//         n /=10;
+//     }
+
+//     cout << rev;
+//     return 0;
+// }
+
+// 2. GCD and LCM 
