61ce7a926fb0f5ac83a30c3f2bf38990f0d67647
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2025-09-01 19:27:48 +0600

    GCD and LCM problem solved

--- Practice.cpp
+++ Practice.cpp
@@ -1,4 +1,4 @@
-// 1. Reverse problem 
+// 1. Reverse problem
 
 // #include<bits/stdc++.h>
 // using namespace std;
@@ -17,4 +17,27 @@
 //     return 0;
 // }
 
-// 2. GCD and LCM 
+// 2. GCD and LCM
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    int num1, num2, small, res, GCD =1;
+    cin >> num1 >> num2;
+    small = num1 < num2 ? num1 : num2;
+
+    for(int i= 1; i<= small; i++){
+        if(num1 % i == 0 && num2 % i == 0){
+            GCD = i;
+        }
+    }
+
+    int LCM = (num1 * num2) / GCD ;
+
+    cout <<"GCD: " << GCD <<endl;
+    cout <<"LCM: " << LCM <<endl;
+
+    return 0;
+}
