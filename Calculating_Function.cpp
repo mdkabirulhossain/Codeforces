fa492a4489460c84d0aab49047e72d0378ec6eb7
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2025-09-24 19:45:30 +0600

    Calculating problem is updated

--- Calculating_Function.cpp
+++ Calculating_Function.cpp
@@ -1,18 +1,21 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    int n, sum=0;
+int main()
+{
+    long long n, sum = 0;
     cin >> n;
-    for(int i=0; i<n; i++){
-        if(i%2 == 0){
-            sum -= i+1;
-        }else{
-            sum += i+1;
-        }
+
+    if (n % 2 == 0)
+    {
+        sum = n / 2;
+    }
+    else
+    {
+        sum = -(n + 1) / 2;
     }
 
-    cout <<sum <<endl;
+    cout << sum << endl;
 
     return 0;
 }
