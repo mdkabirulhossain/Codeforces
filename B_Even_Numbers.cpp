377583445a40ebb69e5ca9818bf09e08587b974e
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2025-09-25 17:20:42 +0600

    Even Number problem is solved

---
+++ B_Even_Numbers.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n, flag = 0;
+    cin >> n;
+    for(int i=1; i<=n; i++){
+        if(i%2 == 0){
+            cout << i <<endl;
+            flag ++;
+        }
+    }
+    if(flag == 0){
+        cout << -1 <<endl;
+    }
+
+    return 0;
+}
