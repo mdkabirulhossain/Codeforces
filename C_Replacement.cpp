bc14108a75b2e703c17fc28116cd3543df3cf4e5
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2025-09-26 17:12:13 +0600

    Replacement problem is solved

---
+++ C_Replacement.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    int arr[n];
+
+    for(int i=0; i<n; i++){
+        cin >> arr[i];
+    }
+
+    for(int j=0; j<n; j++){
+        if(arr[j] < 0){
+            arr[j] = 2;
+        }else if(arr[j] > 0){
+            arr[j] = 1;
+        }else{
+            arr[j] = 0;
+        }
+    }
+
+    for(int k=0; k<n; k++){
+        cout << arr[k]<< " ";
+    }
+    
+    return 0;
+}
