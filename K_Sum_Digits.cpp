ce5f796ac75b1b9ac726582061fe5989901fd6dd
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2025-10-04 21:31:34 +0600

    K Sum Digits problem is solved

---
+++ K_Sum_Digits.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n, sum=0;
+    string s;
+    cin >> n >> s;
+    
+    for(int i=0; i<n; i++){
+        sum +=s[i]-'0';
+    }
+
+    cout << sum <<endl;
+    
+    return 0;
+}
