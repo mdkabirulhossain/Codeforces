33f7ef3e113e6492532e915d585356916d9bb1a6
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2025-10-31 21:36:40 +0600

    Object Sort problem is solved

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,10 @@
+   // for (int i = 0; i < n - 1; i++)
+    // {
+    //     for (int j = i + 1; j < n; j++)
+    //     {
+    //         if (arr[i].marks < arr[j].marks)
+    //         {
+    //             swap(arr[i], arr[j]);
+    //         }
+    //     }
+    // }
