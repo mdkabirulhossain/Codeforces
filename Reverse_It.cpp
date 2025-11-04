b921a321d34c814e536b250325492343f1171ace
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2025-11-04 16:31:37 +0600

    Class Object Reverse It problem solved

---
+++ Reverse_It.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Student{
+    public:
+        string nm;
+        int cls;
+        char s;
+        int id;
+};
+
+int main(){
+    int n;
+    cin>> n;
+    Student* a = new Student[n];
+
+    for(int i=0; i<n; i++){
+        cin>> a[i].nm >> a[i].cls >> a[i].s >> a[i].id;
+    }
+
+     for(int i=0; i<n; i++){
+        cout << a[i].nm <<" " << a[i].cls<<" "<< a[n-1-i].s  <<" "<< a[i].id<<endl;
+    }
+    return 0;
+}
