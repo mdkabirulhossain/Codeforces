08d2d3133c101b30e21ffdb6d71c0023440a26fa
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2025-11-11 22:19:24 +0600

    Copy all the value from array to Vector

---
+++ Vector/copyArrayToVector.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i=0; i<n; i++){
+        cin >> arr[i];
+    }
+
+    vector<int> v(arr, arr+n);
+
+    for(int i:v){
+        cout << i << " ";
+    }
+
+    return 0;
+}
--- Vector/input.cpp
+++ Vector/input.cpp
@@ -4,11 +4,13 @@ using namespace std;
 int main(){
     int n;
     cin >> n;
-    vector<int>v(n);
+    // vector<int>v(n);
 
-    for(int i=0; i<n; i++){
-        cin >> v[i];
-    }
+    // for(int i=0; i<n; i++){
+    //     cin >> v[i];
+    // }
+
+    vector<int>v(n, 100);
 
     for(int i:v){
         cout << i <<endl;
